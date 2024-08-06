0b784375007c0a4ee2b4a3fe73e373c211a819e2
goti jemish <gja5364@gmail.com>
2024-08-06 13:11:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const path = require("path");
+
+//dot config
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//routes
+// 1 test route
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/inventory", require("./routes/inventoryRoutes"));
+app.use("/analytics", require("./routes/analyticsRoutes"));
+app.use("/admin", require("./routes/adminRoutes"));
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running on Port ${PORT}`.bgBlue.white
+  );
+});
